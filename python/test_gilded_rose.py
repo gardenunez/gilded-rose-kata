2af70c6c90da33423290c77898e3dec41d6eccec
jorge jardines <gardenunez@gmail.com>
2018-02-26 00:18:44 +0100

    Add test rules
    "Backstage passes", like aged brie, increases in quality as it's sell-in value approaches; quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but quality drops to 0 after the concert
    Replace AssertEquals by AssertEqual

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,6 +5,7 @@ from gilded_rose import Item, GildedRose
 
 AGED_BRIE = "Aged Brie"
 SULFURAS = "Sulfuras, Hand of Ragnaros"
+BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
 
 class GildedRoseTest(unittest.TestCase):
     ITEM_NAME = 'an_item_name'
@@ -14,9 +15,9 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item(self.ITEM_NAME, sell_in, quality)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals(self.ITEM_NAME, items[0].name)
-        self.assertEquals(sell_in - 1, items[0].sell_in)
-        self.assertEquals(quality - 1, items[0].quality)
+        self.assertEqual(self.ITEM_NAME, items[0].name)
+        self.assertEqual(sell_in - 1, items[0].sell_in)
+        self.assertEqual(quality - 1, items[0].quality)
 
     def test_sell_date_passes_quality_degrade_twice(self):
         sell_in = 0
@@ -24,8 +25,8 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item(self.ITEM_NAME, sell_in, quality)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals(sell_in - 1, items[0].sell_in)
-        self.assertEquals(quality - 2, items[0].quality)
+        self.assertEqual(sell_in - 1, items[0].sell_in)
+        self.assertEqual(quality - 2, items[0].quality)
 
     def test_quality_is_never_negative(self):
         sell_in = 0
@@ -33,8 +34,8 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item(self.ITEM_NAME, sell_in, quality)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals(sell_in - 1, items[0].sell_in)
-        self.assertEquals(quality, items[0].quality)
+        self.assertEqual(sell_in - 1, items[0].sell_in)
+        self.assertEqual(quality, items[0].quality)
 
     def test_aged_brie_increase_quality(self):
         sell_in = 1
@@ -42,8 +43,8 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item(AGED_BRIE, sell_in, quality)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals(sell_in - 1, items[0].sell_in)
-        self.assertEquals(quality + 1, items[0].quality)
+        self.assertEqual(sell_in - 1, items[0].sell_in)
+        self.assertEqual(quality + 1, items[0].quality)
 
     def test_quality_not_more_than_50(self):
         sell_in = 2
@@ -51,8 +52,8 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item(AGED_BRIE, sell_in, quality)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals(sell_in - 1, items[0].sell_in)
-        self.assertEquals(quality, items[0].quality)
+        self.assertEqual(sell_in - 1, items[0].sell_in)
+        self.assertEqual(quality, items[0].quality)
 
     def test_sulfuras_never_has_to_be_sold_nor_decrease_in_quality(self):
         sell_in = 2
@@ -60,8 +61,44 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item(SULFURAS, sell_in, quality)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals(sell_in, items[0].sell_in)
-        self.assertEquals(quality, items[0].quality)
+        self.assertEqual(sell_in, items[0].sell_in)
+        self.assertEqual(quality, items[0].quality)
+
+    def test_backstage_increase_quality(self):
+        sell_in = 12
+        quality = 10
+        items = [Item(BACKSTAGE_PASS, sell_in, quality)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(sell_in - 1, items[0].sell_in)
+        self.assertEqual(quality + 1, items[0].quality)
+
+    def test_backstage_increase_quality_by_2(self):
+        for sell_in in range(6, 11):
+            quality = 1
+            items = [Item(BACKSTAGE_PASS, sell_in, quality)]
+            gilded_rose = GildedRose(items)
+            gilded_rose.update_quality()
+            self.assertEqual(sell_in - 1, items[0].sell_in)
+            self.assertEqual(quality + 2, items[0].quality)
+
+    def test_backstage_increase_quality_by_3(self):
+        for sell_in in range(1, 5):
+            quality = 1
+            items = [Item(BACKSTAGE_PASS, sell_in, quality)]
+            gilded_rose = GildedRose(items)
+            gilded_rose.update_quality()
+            self.assertEqual(sell_in - 1, items[0].sell_in)
+            self.assertEqual(quality + 3, items[0].quality)
+
+    def test_backstage_quality_drops_after_concert(self):
+        sell_in = 0
+        quality = 1
+        items = [Item(BACKSTAGE_PASS, sell_in, quality)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(sell_in - 1, items[0].sell_in)
+        self.assertEqual(0, items[0].quality)
 
 if __name__ == '__main__':
     unittest.main()
